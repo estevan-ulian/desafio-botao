a3393a009816c2372f3c496ae1a3bd1ac1f37b79
Estevan Ulian <estevan.ulian@gmail.com>
2024-10-27 16:34:56 -0300

    feat: add the ToogleTheme component

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -4,10 +4,12 @@ import { links } from "../constants/links";
 
 import { Heart } from "lucide-react";
 import { Typography } from "./ui/typography";
+import { ToggleTheme } from "./ui/toggle-theme";
 
 export function Footer() {
 	return (
-		<footer className="flex flex-col-reverse sm:flex-row gap-2 items-center justify-between max-w-screen-xl px-4 mx-auto w-full py-2">
+		<footer className="flex flex-col sm:flex-row gap-2 items-center justify-between max-w-screen-xl px-4 mx-auto w-full py-2">
+			<ToggleTheme />
 			<Typography variant="smallText" colors="muted">
 				&copy; 2024 - Todos os direitos reservados
 			</Typography>

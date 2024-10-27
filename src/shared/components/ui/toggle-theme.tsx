8de86a52363b7c575fc0a8fc9f56c62c1f033c5d
Estevan Ulian <estevan.ulian@gmail.com>
2024-10-27 16:33:48 -0300

    refactor: tweaks ToogleTheme component styles

--- src/shared/components/ui/toggle-theme.tsx
+++ src/shared/components/ui/toggle-theme.tsx
@@ -17,9 +17,14 @@ export function ToggleTheme() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
-					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+				<Button
+					variant="ghost"
+					size="default"
+					className="p-0 px-2 text-muted-foreground"
+				>
+					<Sun className="h-[1.2rem] w-[1.2rem] dark:absolute dark:invisbile dark:opacity-0 dark:size-0" />
+					<Moon className="h-[1.2rem] w-[1.2rem] absolute invisible opacity-0 dark:static dark:visible dark:opacity-100" />
+					Alternar Tema
 					<span className="sr-only">Alternar tema</span>
 				</Button>
 			</DropdownMenuTrigger>

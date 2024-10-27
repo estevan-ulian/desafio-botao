ac5d7a0c00e83506b2b5fa0c99aa7867b50ecf09
Estevan Ulian <estevan.ulian@gmail.com>
2024-10-27 12:27:16 -0300

    fix: adjusts font-family in tailwindconfig file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
 	darkMode: ["class"],
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
+		fontFamily: {
+			sans: ["var(--font-sans)", ...fontFamily.sans],
+		},
 		extend: {
 			borderRadius: {
 				lg: "var(--radius)",

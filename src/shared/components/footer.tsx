de1cc512384aa41090fe81bbc8bdad8a87d8c1d8
Estevan Ulian <estevan.ulian@gmail.com>
2024-10-27 12:27:49 -0300

    feat: creates Footer component

---
+++ src/shared/components/footer.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import { links } from "../constants/links";
+
+import { Heart } from "lucide-react";
+import { Typography } from "./ui/typography";
+
+export function Footer() {
+	return (
+		<footer className="flex flex-col-reverse sm:flex-row gap-2 items-center justify-between max-w-screen-xl px-4 mx-auto w-full py-2">
+			<Typography variant="smallText" colors="muted">
+				&copy; 2024 - Todos os direitos reservados
+			</Typography>
+			<Typography
+				variant="smallText"
+				colors="muted"
+				className="flex gap-1 items-center"
+			>
+				Criado com <Heart className="size-4 text-destructive" /> por{" "}
+				<Link target="_blank" href={links.author}>
+					Estevan Ulian
+				</Link>
+			</Typography>
+		</footer>
+	);
+}

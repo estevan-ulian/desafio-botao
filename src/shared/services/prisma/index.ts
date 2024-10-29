2666c08ffad56cbbeeba03258ba4c2dfa900bc74
Estevan Ulian <estevan.ulian@gmail.com>
2024-10-29 11:27:26 -0300

    refactor: update import in scene route.ts and prisma-client/index.ts

--- src/shared/services/prisma/index.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export class PrismaClientService {
-	constructor(readonly prisma: PrismaClient) {}
-
-	static create(prisma: PrismaClient) {
-		return new PrismaClientService(prisma);
-	}
-}

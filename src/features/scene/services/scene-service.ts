9961b58e95e1e2927b9b8ab88e3ee9a3755e44d6
Estevan Ulian <estevan.ulian@gmail.com>
2024-10-30 20:47:30 -0300

    refactor: replace SceneService class with functional sceneService using PrismaClient

--- src/features/scene/services/scene-service.ts
+++ src/features/scene/services/scene-service.ts
@@ -1,28 +1,102 @@
-import { HttpClient } from "@/shared/services/http/http-client";
-import { IQuestion } from "../types/question-type";
-import { ISceneService } from "../types/scene-service-types";
+import {
+	ICreateSceneActionResponse,
+	IGetSceneServiceResponse,
+} from "../types/scene-actions-type";
+import { IQuestionCreateBody } from "../types/question-type";
+import { PrismaClient } from "@prisma/client";
 
-export class SceneService implements ISceneService {
-	constructor(readonly api: HttpClient) {
-		if (!api) throw new TypeError("Instance of API Adapter is required");
-	}
+export interface ISceneService {
+	create: (
+		scenario: IQuestionCreateBody,
+	) => Promise<ICreateSceneActionResponse>;
+	findOne: (id: string) => Promise<IGetSceneServiceResponse>;
+}
 
-	async create(scene: IQuestion): Promise<{ id: string }> {
-		const httpResponse = await this.api.request<IQuestion, { id: string }>({
-			method: "POST",
-			url: "/scene",
-			body: scene,
-		});
+export function sceneService(api: PrismaClient): ISceneService {
+	return {
+		create: async (
+			scenario: IQuestionCreateBody,
+		): Promise<ICreateSceneActionResponse> => {
+			const { data } = scenario;
 
-		return httpResponse.data;
-	}
+			if (!data.text) {
+				return {
+					data: null,
+					error: {
+						message: "Insira um texto para a pergunta.",
+					},
+				};
+			}
 
-	async findOne(id: string): Promise<IQuestion> {
-		const httpResponse = await this.api.request<null, IQuestion>({
-			method: "GET",
-			url: `/scene?id=${id}`,
-		});
+			if (!data.confirmationText) {
+				return {
+					data: null,
+					error: {
+						message: "Insira um texto de confirmação.",
+					},
+				};
+			}
 
-		return httpResponse.data;
-	}
+			if (!data.answers) {
+				return {
+					data: null,
+					error: {
+						message: "Não há respostas para a pergunta.",
+					},
+				};
+			}
+
+			if (data.answers.length !== 2) {
+				return {
+					data: null,
+					error: {
+						message: "As perguntas devem ter 2 respostas.",
+					},
+				};
+			}
+
+			const question = await api.question.create({
+				data: {
+					text: data.text,
+					confirmationText: data.confirmationText,
+					answers: {
+						create: data.answers,
+					},
+				},
+			});
+
+			return { data: { id: question.id }, error: null };
+		},
+
+		findOne: async (id: string): Promise<IGetSceneServiceResponse> => {
+			const question = await api.question.findUnique({
+				where: {
+					id,
+				},
+				include: {
+					answers: true,
+				},
+			});
+
+			if (!question?.id) {
+				return {
+					data: null,
+					error: {
+						message: "Cenário não encontrado ou já foi excluído.",
+					},
+				};
+			}
+
+			return {
+				data: {
+					id: question.id,
+					text: question.text,
+					confirmationText: question.confirmationText,
+					createdAt: question.createdAt,
+					answers: question.answers,
+				},
+				error: null,
+			};
+		},
+	};
 }

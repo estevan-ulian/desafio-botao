c477177824209a54d4f2760df15d96df43dc95d7
Estevan Ulian <estevan.ulian@gmail.com>
2024-10-30 20:46:33 -0300

    refactor: remove HTTP client and related repository interfaces

--- src/shared/services/http/repository/http-request-repository.ts
+++
@@ -1,8 +0,0 @@
-import { IHttpMethod } from "./http-method-repository";
-
-export interface IHttpRequest<B> {
-	url: string;
-	method?: IHttpMethod;
-	headers?: Record<string, string>;
-	body?: B;
-}

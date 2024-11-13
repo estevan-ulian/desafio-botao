5714f2b81a827a8c48e37c24f1519907711f536e
Estevan Ulian <estevan.ulian@gmail.com>
2024-11-13 09:02:51 -0300

    feat: implement HttpClient service with request handling and error management

---
+++ src/shared/services/http-client/repository/http-error-repository.ts
@@ -0,0 +1,7 @@
+export interface IHttpError {
+	name: string;
+	message: string;
+	status: number;
+	duration: number;
+	details: string[];
+}
---
+++ src/shared/services/http-client/repository/http-request-repository.ts
@@ -0,0 +1,8 @@
+import { IHttpMethod } from "./http-method-repository";
+
+export interface IHttpRequest<B> {
+	path: string;
+	method?: IHttpMethod;
+	headers?: Record<string, string>;
+	body?: B;
+}

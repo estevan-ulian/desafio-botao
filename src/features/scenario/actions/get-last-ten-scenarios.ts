275c478ce14bde0985f0460c2972071acfd01466
Estevan Ulian <estevan.ulian@gmail.com>
2025-01-01 20:32:43 -0300

    feat: add revalidation for scenario cache in getLastTenScenariosAction

--- src/features/scenario/actions/get-last-ten-scenarios.ts
+++ src/features/scenario/actions/get-last-ten-scenarios.ts
@@ -3,9 +3,11 @@
 import { prisma } from "@/shared/services/prisma";
 import { scenarioService } from "../services/scenario-service";
 import { IGetLastTenScenariosServiceResponse } from "../types/scenario-actions-type";
+import { revalidateTag } from "next/cache";
 
 const service = scenarioService(prisma);
 
 export async function getLastTenScenariosAction(): Promise<IGetLastTenScenariosServiceResponse> {
+    revalidateTag("scenario");
     return await service.findLastTen();
 }

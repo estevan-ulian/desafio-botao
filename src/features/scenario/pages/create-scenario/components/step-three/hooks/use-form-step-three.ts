69b10a029526bf21b9bfb8ccfa166577c72cc240
Estevan Ulian <estevan.ulian@gmail.com>
2025-01-03 17:29:07 -0300

    feat: enhance validation for confirmation content in step three form

--- src/features/scenario/pages/create-scenario/components/step-three/hooks/use-form-step-three.ts
+++ src/features/scenario/pages/create-scenario/components/step-three/hooks/use-form-step-three.ts
@@ -11,43 +11,61 @@ import { GiphyService } from "@/features/scenario/services/giphy-service";
 import { HttpClient } from "@/shared/services/http-client";
 
 const formStepThreeSchema = z.object({
-    data: z.object({
-        confirmation: z
-            .object({
+    data: z
+        .object({
+            confirmation: z.object({
                 confirmationType: z.enum(["", "text", "image", "video", "gif"]),
                 confirmationContent: z.string(),
-            })
-            .superRefine((arg, ctx) => {
-                const isVideo = arg.confirmationType === "video";
-                const isText = arg.confirmationType === "text";
-                const isGif = arg.confirmationType === "gif";
+            }),
+        })
+        .superRefine((arg, ctx) => {
+            const isVideo = arg.confirmation.confirmationType === "video";
+            const isText = arg.confirmation.confirmationType === "text";
+            const isGif = arg.confirmation.confirmationType === "gif";
 
+            if (!arg.confirmation.confirmationType) {
+                return ctx.addIssue({
+                    code: ZodIssueCode.custom,
+                    message: "Selecione o tipo de confirmação",
+                    path: ["confirmation", "confirmationType"],
+                });
+            }
+
+            if (isText && arg.confirmation.confirmationContent.length < 10) {
+                return ctx.addIssue({
+                    code: ZodIssueCode.custom,
+                    message: "O texto deve ter no mínimo 10 caracteres",
+                    path: ["confirmation", "confirmationContent"],
+                });
+            }
+
+            if (
+                !isText &&
+                !isGif &&
+                !arg.confirmation.confirmationContent.startsWith("https://")
+            ) {
+                return ctx.addIssue({
+                    code: ZodIssueCode.custom,
+                    message: "Insira um endereço de URL válido!",
+                    path: ["confirmation", "confirmationContent"],
+                });
+            }
+
+            if (isVideo) {
                 if (
-                    !isText &&
-                    !isGif &&
-                    !arg.confirmationContent.startsWith("https://")
+                    !arg.confirmation.confirmationContent.includes(
+                        "youtube.com"
+                    ) &&
+                    !arg.confirmation.confirmationContent.includes("youtu.be")
                 ) {
-                    ctx.addIssue({
+                    return ctx.addIssue({
                         code: ZodIssueCode.custom,
-                        message: "Insira um endereço de URL válido!",
-                        path: ["confirmationContent"],
+                        message: "Somente vídeos do YouTube são permitos!",
+                        path: ["confirmation", "confirmationContent"],
                     });
                 }
-
-                if (isVideo) {
-                    if (
-                        !arg.confirmationContent.includes("youtube.com") &&
-                        !arg.confirmationContent.includes("youtu.be")
-                    ) {
-                        ctx.addIssue({
-                            code: ZodIssueCode.custom,
-                            message: "Somente vídeos do YouTube são permitos!",
-                            path: ["confirmationContent"],
-                        });
-                    }
-                }
-            }),
-    }),
+            }
+        }),
 });
 
 export type FormStepThreeData = z.infer<typeof formStepThreeSchema>;
@@ -63,6 +81,8 @@ export function useFormStepThree() {
         resolver: zodResolver(formStepThreeSchema),
     });
 
+    console.log(form.formState.errors);
+
     const [searchTerm, setSearchTerm] = React.useState("");
     const [selectedGif, setSelectedGif] = React.useState<
         IGiphyData | undefined
@@ -74,7 +94,7 @@ export function useFormStepThree() {
 
     const httpClient = HttpClient(
         axios,
-        process.env.NEXT_PUBLIC_GIPHY_BASE_URL!,
+        process.env.NEXT_PUBLIC_GIPHY_BASE_URL!
     );
     const giphyService = GiphyService(httpClient);
 
@@ -86,7 +106,7 @@ export function useFormStepThree() {
     };
 
     const handleGifSelect = (
-        gif: React.SetStateAction<IGiphyData | undefined>,
+        gif: React.SetStateAction<IGiphyData | undefined>
     ) => {
         setSelectedGif(gif);
         setIsSearchOpen(false);
@@ -103,7 +123,7 @@ export function useFormStepThree() {
         if (selectedGif) {
             return form.setValue(
                 "data.confirmation.confirmationContent",
-                selectedGif.images.downsized.url,
+                selectedGif.images.downsized.url
             );
         }
 
@@ -128,7 +148,7 @@ export function useFormStepThree() {
         if (form.getValues("data.confirmation.confirmationType") === "gif") {
             if (!selectedGif)
                 return form.setError("data.confirmation.confirmationType", {
-                    message: "Selecione um GIF",
+                    message: "Pesquise e selecione um GIF",
                 });
         }
         next(data);
